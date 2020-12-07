c00faf1b99329224a08d318f3c1847f31db98c34
Alexander Todorov <aleksandar9999a@gmail.com>
2020-12-07 07:14:10 +0200

    Fix syntax

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -9,6 +9,6 @@ export class DataService {
 		{ id: 5, title: 'TSX', content: 'It\'s actually JSX. ExF delivers a familiar syntax such as React.' },
 		{ id: 1, title: 'Shadow DOM', content: 'Important part of ExF is encapsulation and here the Shadow DOM intervenes.' },
 		{ id: 3, title: 'Virtual DOM', content: 'To make things easier, more sustainable and more dynamic, ExF uses Virtual Dom and updates only what is necessary.' },
-		{ id: 4, title: 'VIrtual Styles', content: 'This is not something you could find on Google. Since it turned out to be great to be able to generate a Elements and then update them dynamically, so why not do the same with Styles?' },
+		{ id: 4, title: 'Virtual Styles', content: 'This is not something you could find on Google. Since it turned out to be great to be able to generate a Elements and then update them dynamically, so why not do the same with Styles?' },
 	]
 }
